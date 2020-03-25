c4a8bd51b44f8f6ce410056b9e04f2d650c06ff6
Daniel Rigberg <daniel@lonerooftop.com>
2020-03-25 21:06:27 +0100

    fish swim: midpoint

---
+++ Assets/Scripts/FishSwim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSwim : MonoBehaviour
+{
+    public Transform[] bones;
+    public float frequency;
+    public GameObject fish;
+    public float maxRotateDegreesPerBone;
+    private float startTime;
+
+    void Start() {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float elapsed = Time.time - startTime;
+        float targetRotation = Mathf.Sin(elapsed * frequency * Mathf.PI) * maxRotateDegreesPerBone;
+        for (int i = 0; i < bones.Length; i++) {
+            // Vector3 targetVector = Quaternion.AngleAxis(targetRotation,fish.forward);
+            // FishMovementUtils.TurnToFace(rb, transform, targetDirection);
+            float currentRotation = bones[i].rotation.z;
+            bones[i].Rotate(0, 0, targetRotation - currentRotation);
+        }
+    }
+}
