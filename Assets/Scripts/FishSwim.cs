87281b98181b41f7baecf7fde6957125f5493e7e
Daniel Rigberg <daniel@lonerooftop.com>
2020-03-25 21:27:22 +0100

    move rotation to FixedUpdate()

--- Assets/Scripts/FishSwim.cs
+++ Assets/Scripts/FishSwim.cs
@@ -9,6 +9,10 @@ public class FishSwim : MonoBehaviour
     public GameObject fish;
     public float maxRotateDegreesPerBone;
     private float startTime;
+    private float elapsed;
+    private float targetRotation = 0.0f;
+    private float lastRotation;
+    private float rotationDiff;
 
     void Start() {
         startTime = Time.time;
@@ -17,13 +21,16 @@ public class FishSwim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float elapsed = Time.time - startTime;
-        float targetRotation = Mathf.Sin(elapsed * frequency * Mathf.PI) * maxRotateDegreesPerBone;
+        elapsed = Time.time - startTime;
+        lastRotation = targetRotation;
+        targetRotation = Mathf.Sin(elapsed * frequency * Mathf.PI) * maxRotateDegreesPerBone;
+        rotationDiff = targetRotation - lastRotation;
+        Debug.Log("Target: " + targetRotation + ", Diff: " + rotationDiff);
+    }
+
+    void FixedUpdate() {
         for (int i = 0; i < bones.Length; i++) {
-            // Vector3 targetVector = Quaternion.AngleAxis(targetRotation,fish.forward);
-            // FishMovementUtils.TurnToFace(rb, transform, targetDirection);
-            float currentRotation = bones[i].rotation.z;
-            bones[i].Rotate(0, 0, targetRotation - currentRotation);
+            bones[i].Rotate(0, 0, rotationDiff);
         }
     }
 }
