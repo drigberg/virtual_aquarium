4232103eb6dff43c9136ba3261950fbfeff95f66
Daniel Rigberg <daniel@lonerooftop.com>
2020-04-05 15:51:59 +0200

    let Jimmy flap his wings

--- Assets/Scripts/StingraySwim.cs
+++ Assets/Scripts/StingraySwim.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class StingraySwim : MonoBehaviour
 {
+    public Transform[] bones;
+    public float frequency;
+    public float maxRotateDegreesPerBone;
+    private float startTime;
+    private float elapsed;
+    private float targetRotation = 0.0f;
+    private float lastRotation;
+    private float rotationDiff;
+    private float offset;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        startTime = Time.time;
+        offset = Random.Range(Mathf.PI * -1, Mathf.PI);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        elapsed = Time.time - startTime;
+        lastRotation = targetRotation;
+        targetRotation = Mathf.Sin(elapsed * frequency * Mathf.PI + offset) * maxRotateDegreesPerBone;
+    }
+
+    void FixedUpdate() {
+        for (int i = 0; i < bones.Length; i++) {
+            bones[i].localEulerAngles = new Vector3(targetRotation, 0, 0);
+        }
     }
 }
