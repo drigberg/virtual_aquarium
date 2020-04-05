1c0046cbca5b87e87e10c2bb7dbcb4dba9d0d0c9
Daniel Rigberg <daniel@lonerooftop.com>
2020-04-05 17:52:08 +0200

    add hovering to Jimmy

--- Assets/Scripts/BottomFeederPatrol.cs
+++ Assets/Scripts/BottomFeederPatrol.cs
@@ -7,8 +7,11 @@ public class BottomFeederPatrol : MonoBehaviour
     public Rigidbody rb;
     public Transform minXZ;
     public Transform maxXZ;
+    public float hoverHeight = 2.0f;
+    public float restingHoverHeight = 0.5f;
     public float targetRadius = 3.0f;
     public float restProbability = 0.5f;
+    public float minRestTimeSeconds = 5.0f;
     public float maxRestTimeSeconds = 15.0f;
     public float speed = 5.0f;
     public Vector3 currentForce;
@@ -30,6 +33,7 @@ public class BottomFeederPatrol : MonoBehaviour
     {
         if (restTimer > 0.0f) {
             IterateRestTimer();
+            currentForce = AdjustAltitude(restingHoverHeight);
             return;
         }
 
@@ -49,7 +53,10 @@ public class BottomFeederPatrol : MonoBehaviour
     }
 
     bool HaveReachedDestination(Vector3 destination) {
-        return Vector3.Distance(destination, transform.position) < targetRadius;
+        // check distance to point along XZ plane
+        Vector3 destinationXZ = new Vector3(destination.x, 0, destination.z);
+        Vector3 transformXZ = new Vector3(transform.position.x, 0, transform.position.z);
+        return Vector3.Distance(destinationXZ, transformXZ) < targetRadius;
     }
 
     Vector3 GetNextTarget() {
@@ -68,7 +75,7 @@ public class BottomFeederPatrol : MonoBehaviour
     }
 
     float GetRestInterval() {
-        return Random.Range(0, maxRestTimeSeconds);
+        return Random.Range(minRestTimeSeconds, maxRestTimeSeconds);
     }
 
     void IterateRestTimer() {
@@ -79,9 +86,30 @@ public class BottomFeederPatrol : MonoBehaviour
         return Random.Range(0, 1) < restProbability;
     }
 
+    float GetDistanceToTerrain() {
+        RaycastHit hit;
+        Ray downRay = new Ray(transform.position, -Vector3.up);
+        if (Physics.Raycast(downRay, out hit)) {
+            return hit.distance;
+        }
+        // assume at least 5 units away
+        return 5.0f;
+    }
+
+    Vector3 AdjustAltitude(float targetAltitude) {
+        // stay close to the terrain along the Y axis
+        float distanceToTerrain = GetDistanceToTerrain();
+        float hoverError = distanceToTerrain - targetAltitude;
+        float directionError = Mathf.Abs(hoverError) / hoverError;
+        return FishMovementUtils.MoveTowardsTarget(rb, -Vector3.up, speed * directionError * 0.5f);
+    }
+
     void MoveTowardsTarget() {
+        // move towards target along XZ plane
         Vector3 targetDirection = currentTarget - transform.position;
         currentForce = FishMovementUtils.MoveTowardsTarget(rb, targetDirection, speed);
+        currentForce += AdjustAltitude(hoverHeight);
+
         // face midway between current velocity vector and target: like anticipating a turn
         Vector3 facingDirection = (rb.velocity + targetDirection) / 2;
         FishMovementUtils.TurnToFace(rb, transform, facingDirection);
