68af807afbd219536c43e61234cd3c4467561df2
Daniel Rigberg <daniel@lonerooftop.com>
2020-04-17 15:02:01 +0200

    add pause functionality with menu and instructions

--- Assets/Scripts/GUI/GUI.cs
+++ Assets/Scripts/GUI/GUI.cs
@@ -4,21 +4,53 @@ using UnityEngine;
 
 public class GUI : MonoBehaviour
 {
+    public Canvas canvas;
+    public bool paused = false;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-
-    void OnGUI()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
+    void Start() {
+        PauseGame();
+        paused = true;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            TogglePaused();
+        }
+    }
+
+    
+    void OnGUI() {
+        Cursor.lockState = CursorLockMode.Locked;
+        if (GUILayout.Button("Instructions")) {
+            Debug.Log("Instructions!");
+        }
+    }
+
+    void OnApplicationFocus(bool hasFocus) {
+        if (!hasFocus) {
+            PauseGame();
+        }
+    }
+
+    void TogglePaused() {
+        if (paused) {
+            UnpauseGame();
+        } else {
+            PauseGame();
+        }
+    }
+
+    void PauseGame() {
+        Time.timeScale = 0;
+        canvas.enabled = true;
+        paused = true;
+    }
+
+    void UnpauseGame() {
+        Time.timeScale = 1;
+        canvas.enabled = false;
+        paused = false;
     }
 }
