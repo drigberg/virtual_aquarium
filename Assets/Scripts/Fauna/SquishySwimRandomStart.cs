8f31edf2b63a413dce9b138f9394a0b27f28d393
Daniel Rigberg <daniel.rigberg@gmail.com>
2020-11-03 12:42:45 +0100

    complete Squishy, add swarms to scene

---
+++ Assets/Scripts/Fauna/SquishySwimRandomStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class SquishySwimRandomStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Animator animator = GetComponent <Animator> ();
+        float randomFrame = Random.Range(0f, 1f);
+        for (int i = 0; i < 6; i++) {
+            AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(i);
+            animator.Play(state.fullPathHash, -1, randomFrame);
+        }
+    }
+}
